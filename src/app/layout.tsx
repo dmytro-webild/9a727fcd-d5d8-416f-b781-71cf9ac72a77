8e899556989ca838824ba1ef23ec633bfe573f31
dk <kudin.dima@gmail.com>
2026-05-08 11:51:12 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'B115 CitiPoint Zayed Heights | الشيخ زايد',
+  description: 'امتلك شقتك الفاخرة في B115 CitiPoint Zayed Heights. موقع متميز، أمان، وأفضل الأسعار في الشيخ زايد. تواصل معنا للحجز والاستفسار.',
+  openGraph: {
+    "title": "CitiPoint Zayed Heights",
+    "description": "أرقى المشاريع السكنية في الشيخ زايد.",
+    "siteName": "CitiPoint"
+  },
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
